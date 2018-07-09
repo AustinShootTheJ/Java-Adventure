ffc00656713bdb057f1ff53b173cfc654c853d9a
AustinShootTheJ <austinjmccune@gmail.com>
2018-07-09 11:13:31 -0400

    Made Location class immutable.

--- src/com/example/adventure/Main.java
+++ src/com/example/adventure/Main.java
@@ -13,38 +13,41 @@ public class Main {
 
 
         Scanner scanner = new Scanner(System.in);
-
+        Map<String, Integer> tempExit = new HashMap<String, Integer>();
         // populates locations with ints and Location objects. We use the new modifer since no existing Location object is being referenced.
-        locations.put(0, new Location(0,"You are sitting in front of a computer learning Java"));
-        locations.put(1, new Location(1,"You are standing at the end of a road before a small brick building"));
-        locations.put(2, new Location(2,"You are at the top of a hill"));
-        locations.put(3, new Location(3,"You are inside a building, a well house for a small spring"));
-        locations.put(4, new Location(4,"You are in a valley beside a stream"));
-        locations.put(5, new Location(5,"You are in the forest"));
+        locations.put(0, new Location(0,"You are sitting in front of a computer learning Java",null));
 
+
+        tempExit = new HashMap<String, Integer>();
         // exits from room 1
-        locations.get(1).addExit("W",2);
-        locations.get(1).addExit("E",3);
-        locations.get(1).addExit("S",4);
-
+        tempExit.put("W",2);
+        tempExit.put("E",3);
+        tempExit.put("S",4);
+        tempExit.put("N",5);
+        locations.put(1, new Location(1,"You are standing at the end of a road before a small brick building",tempExit));
+        tempExit = new HashMap<String, Integer>();
 
         // exits for room 2
-        locations.get(2).addExit("N",5);
+        tempExit.put("N",5);
+        locations.put(2, new Location(2,"You are at the top of a hill",tempExit));
 
-
+        tempExit = new HashMap<String, Integer>();
         // exits for room 3
-        locations.get(3).addExit("W",1);
+        tempExit.put("W",1);
+        locations.put(3, new Location(3,"You are inside a building, a well house for a small spring",tempExit));
 
-
+        tempExit = new HashMap<String, Integer>();
         // exits for room 4
-        locations.get(4).addExit("N",1);
-        locations.get(4).addExit("W",2);
+        tempExit.put("N",1);
+        tempExit.put("W",2);
+        locations.put(4, new Location(4,"You are in a valley beside a stream",tempExit));
 
-
+        tempExit = new HashMap<String, Integer>();
         // exits for room 5
-        locations.get(5).addExit("S",1);
-        locations.get(5).addExit("W",2);
-
+        tempExit.put("S",1);
+        tempExit.put("W",2);
+        locations.put(5, new Location(5,"You are in the forest",tempExit));
+        
         // Words to check against for directional values.
         Map<String, String> vocabulary = new HashMap<String, String>();
         vocabulary.put("QUIT","Q");
@@ -75,7 +78,7 @@ public class Main {
             if(direction.length() > 1){
                 // splits the string into an array for checking
                 String[] words = direction.split(" ");
-                // checks each word until it finds one in our vocab and assigns direction to the related key. 
+                // checks each word until it finds one in our vocab and assigns direction to the related key.
                 for(String word: words){
                     if(vocabulary.containsKey(word)){
                         direction = vocabulary.get(word);
